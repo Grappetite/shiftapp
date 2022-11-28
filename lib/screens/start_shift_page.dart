76e3c59bb77a03697b662846c5c25a16d9b39f56
grappetite-mahboob <111432368+grappetite-mahboob@users.noreply.github.com>
2022-11-28 14:48:43 +0500

    will pop issue resolve

--- lib/screens/start_shift_page.dart
+++ lib/screens/start_shift_page.dart
@@ -50,7 +50,7 @@ class _StartShiftViewState extends State<StartShiftView> {
 
   String timeElasped = '00:00';
   late Timer _timer;
-
+  bool? willpop = true;
   void startTimer() {
     const oneSec = Duration(seconds: 1);
 
@@ -73,148 +73,116 @@ class _StartShiftViewState extends State<StartShiftView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Column(
-          children: [
-            Image.asset(
-              'assets/images/toplogo.png',
-              height: 20,
-            ),
-            SizedBox(
-              height: 4,
-            ),
-            Text(
-              widget.process.name!,
-              style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus();
-          },
-          child: Column(
+    return WillPopScope(
+      onWillPop: () {
+        return Future.value(willpop);
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Column(
             children: [
-              TimerTopWidget(
-                  selectedShift: widget.selectedShift,
-                  timeElasped: timeElasped),
-              const SizedBox(
-                height: 16,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: ExplainerWidget(
-                  iconName: 'construct',
-                  title: 'Workers',
-                  text1: text1(),
-                  text2: '',
-                  showWarning: true,
-                  showIcon: true,
-                  backgroundColor: lightGreenColor,
-                  postIcon: Icons.check,
-                  postIconColor: Colors.green,
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              ComingSoonContainer(
-                innerWidget: ExplainerWidget(
-                  comingSoon: true,
-                  iconName: 'construct',
-                  title: 'PPE',
-                  text1: '2/5 Planned PPE per Worker Type',
-                  text2: '',
-                  showWarning: true,
-                  showIcon: true,
-                  backgroundColor: lightRedColor,
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 8),
+              Image.asset(
+                'assets/images/toplogo.png',
+                height: 20,
               ),
               SizedBox(
-                height: 8,
+                height: 4,
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Enter Comments',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20.0),
-                      ),
-                      borderSide: BorderSide(color: Colors.black38),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20.0),
-                      ),
-                      borderSide: BorderSide(color: Colors.black38),
-                    ),
+              Text(
+                widget.process.name!,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+            ],
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: Column(
+              children: [
+                TimerTopWidget(
+                    selectedShift: widget.selectedShift,
+                    timeElasped: timeElasped),
+                const SizedBox(
+                  height: 16,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: ExplainerWidget(
+                    iconName: 'construct',
+                    title: 'Workers',
+                    text1: text1(),
+                    text2: '',
+                    showWarning: true,
+                    showIcon: true,
+                    backgroundColor: lightGreenColor,
+                    postIcon: Icons.check,
+                    postIconColor: Colors.green,
                   ),
-                  minLines: 2,
-                  maxLines: 3,
-                  keyboardType: TextInputType.multiline,
-                  controller: _controller,
                 ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width / 1.7,
-                child: TextButton(
-                  onPressed: () async {
-                    if (_controller.text.isEmpty && 1 == 2) {
-                      showAlertDialog(
-                        context: context,
-                        title: 'Error',
-                        message: 'Please add comment.',
-                        actions: [
-                          AlertDialogAction(
-                            label:
-                                MaterialLocalizations.of(context).okButtonLabel,
-                            key: OkCancelResult.ok,
-                          )
-                        ],
-                      );
-                      return;
-                    }
-                    await EasyLoading.show(
-                      status: 'loading...',
-                      maskType: EasyLoadingMaskType.black,
-                    );
-
-                    var result = await WorkersService.addShiftWorker(
-                        widget.shiftId,
-                        widget.processId,
-
-                        ///before at
-                        widget.startTime,
-
-                        ///after
-                        widget.endTime,
-                        widget.userId,
-                        widget.efficiencyCalculation,
-                        _controller.text);
-
-                    await EasyLoading.dismiss();
-
-                    if (result != null) {
-                      if (result.status == 'error') {
+                const SizedBox(
+                  height: 16,
+                ),
+                ComingSoonContainer(
+                  innerWidget: ExplainerWidget(
+                    comingSoon: true,
+                    iconName: 'construct',
+                    title: 'PPE',
+                    text1: '2/5 Planned PPE per Worker Type',
+                    text2: '',
+                    showWarning: true,
+                    showIcon: true,
+                    backgroundColor: lightRedColor,
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8.0),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Enter Comments',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        borderSide: BorderSide(color: Colors.black38),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                        borderSide: BorderSide(color: Colors.black38),
+                      ),
+                    ),
+                    minLines: 2,
+                    maxLines: 3,
+                    keyboardType: TextInputType.multiline,
+                    controller: _controller,
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.7,
+                  child: TextButton(
+                    onPressed: () async {
+                      if (_controller.text.isEmpty && 1 == 2) {
                         showAlertDialog(
                           context: context,
                           title: 'Error',
-                          message: result.message,
+                          message: 'Please add comment.',
                           actions: [
                             AlertDialogAction(
                               label: MaterialLocalizations.of(context)
@@ -223,82 +191,125 @@ class _StartShiftViewState extends State<StartShiftView> {
                             )
                           ],
                         );
-
                         return;
                       }
+                      setState(() {
+                        willpop = false;
+                      });
+                      await EasyLoading.show(
+                        status: 'loading...',
+                        maskType: EasyLoadingMaskType.black,
+                      );
 
-                      if (result.code! == 200) {
-                        final prefs = await SharedPreferences.getInstance();
-                        prefs.setInt('shiftId', widget.selectedShift.id!);
-                        prefs.setInt('processId', widget.processId);
+                      var result = await WorkersService.addShiftWorker(
+                          widget.shiftId,
+                          widget.processId,
 
-                        prefs.setString(
-                            'selectedShiftName', widget.selectedShift.name!);
+                          ///before at
+                          widget.startTime,
 
-                        prefs.setString('selectedShiftStartTime',
-                            widget.selectedShift.startTime!);
+                          ///after
+                          widget.endTime,
+                          widget.userId,
+                          widget.efficiencyCalculation,
+                          _controller.text);
 
-                        prefs.setString('selectedShiftEndTime',
-                            widget.selectedShift.endTime!);
+                      await EasyLoading.dismiss();
 
-                        prefs.setInt('selectedDisplayScreen',
-                            widget.selectedShift.displayScreen!);
+                      if (result != null) {
+                        if (result.status == 'error') {
+                          showAlertDialog(
+                            context: context,
+                            title: 'Error',
+                            message: result.message,
+                            actions: [
+                              AlertDialogAction(
+                                label: MaterialLocalizations.of(context)
+                                    .okButtonLabel,
+                                key: OkCancelResult.ok,
+                              )
+                            ],
+                          );
 
-                        prefs.setInt(
-                            'execute_shift_id', result.data!.executeShiftId!);
-                        if (prefs.getStringList(
-                                result.data!.executeShiftId!.toString()) ==
-                            null) {
-                          List<String> test = [];
-                          var rng = Random();
-                          for (var i = 1; i <= 25; i++) {
-                            test.add(rng.nextInt(100000).toString());
-                            await show(
-                                widget.selectedShift.endDateObject
-                                    .add(Duration(hours: i)),
-                                int.parse(
-                                    result.data!.executeShiftId!.toString() +
-                                        test[i - 1].toString()),
-                                "Did you forget to end your shift?",
-                                "Your scheduled shift ended.  If you've completed today's work please end your shift.");
-                          }
-                          prefs.setStringList(
-                              result.data!.executeShiftId!.toString(), test);
+                          return;
                         }
 
-                        await show(
-                            widget.selectedShift.endDateObject
-                                .subtract(Duration(minutes: 10)),
-                            result.data!.executeShiftId!,
-                            "Don't forget to end your shift!",
-                            "Your shift is about to end, don't forget to register today's work.");
+                        if (result.code! == 200) {
+                          final prefs = await SharedPreferences.getInstance();
+                          prefs.setInt('shiftId', widget.selectedShift.id!);
+                          prefs.setInt('processId', widget.processId);
 
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (BuildContext context) => EndShiftView(
-                                userId: widget.userId,
-                                efficiencyCalculation:
-                                    widget.efficiencyCalculation,
-                                shiftId: widget.shiftId,
-                                processId: widget.processId,
-                                selectedShift: widget.selectedShift,
-                                process: widget.process,
-                                execShiftId: result.data!.executeShiftId!),
-                          ),
-                        );
+                          prefs.setString(
+                              'selectedShiftName', widget.selectedShift.name!);
+
+                          prefs.setString('selectedShiftStartTime',
+                              widget.selectedShift.startTime!);
+
+                          prefs.setString('selectedShiftEndTime',
+                              widget.selectedShift.endTime!);
+
+                          prefs.setInt('selectedDisplayScreen',
+                              widget.selectedShift.displayScreen!);
+
+                          prefs.setInt(
+                              'execute_shift_id', result.data!.executeShiftId!);
+                          if (prefs.getStringList(
+                                  result.data!.executeShiftId!.toString()) ==
+                              null) {
+                            List<String> test = [];
+                            var rng = Random();
+                            for (var i = 1; i <= 25; i++) {
+                              test.add(rng.nextInt(100000).toString());
+                              await show(
+                                  widget.selectedShift.endDateObject
+                                      .add(Duration(hours: i)),
+                                  int.parse(
+                                      result.data!.executeShiftId!.toString() +
+                                          test[i - 1].toString()),
+                                  "Did you forget to end your shift?",
+                                  "Your scheduled shift ended.  If you've completed today's work please end your shift.");
+                            }
+                            prefs.setStringList(
+                                result.data!.executeShiftId!.toString(), test);
+                          }
+
+                          await show(
+                              widget.selectedShift.endDateObject
+                                  .subtract(Duration(minutes: 10)),
+                              result.data!.executeShiftId!,
+                              "Don't forget to end your shift!",
+                              "Your shift is about to end, don't forget to register today's work.");
+
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (BuildContext context) => EndShiftView(
+                                  userId: widget.userId,
+                                  efficiencyCalculation:
+                                      widget.efficiencyCalculation,
+                                  shiftId: widget.shiftId,
+                                  processId: widget.processId,
+                                  selectedShift: widget.selectedShift,
+                                  process: widget.process,
+                                  execShiftId: result.data!.executeShiftId!),
+                            ),
+                          );
+                        }
+                      } else {
+                        EasyLoading.showError('Could not load data');
+                        setState(() {
+                          willpop = true;
+                        });
                       }
-                    } else {
-                      EasyLoading.showError('Could not load data');
-                    }
-                  },
-                  child: Image.asset('assets/images/start_button.png'),
+                    },
+                    child: Image.asset('assets/images/start_button.png'),
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-            ],
+                const SizedBox(
+                  height: 16,
+                ),
+              ],
+            ),
           ),
         ),
       ),
